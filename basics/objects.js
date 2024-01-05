29c5d3963b872cb409e74f2b0802dd85cce11912
unknown <atz.softprgmr@gmail.com>
2024-01-05 21:04:50 +0500

    added changes

---
+++ basics/objects.js
@@ -0,0 +1,16 @@
+// Creating objects
+
+// a. Object Literal Notation:
+
+const obj = {
+    name: "john",
+    age: 30,
+    city: "New York"
+}
+
+// b. Object Constructor:
+
+const person = new Object();
+person.name = "John";
+person.age = 30;
+person.city = "New York";
